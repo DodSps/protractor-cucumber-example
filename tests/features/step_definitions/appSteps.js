86216e2b8393eb57f7b4d1e299ac0b7a3d15e016
Henry Blyth <blyth.henry@gmail.com>
2016-05-10 10:47:14 +0100

    Remove original Givens so copy-pasted will be used

--- tests/features/step_definitions/appSteps.js
+++ tests/features/step_definitions/appSteps.js
@@ -1,14 +1,6 @@
 console.log('appsteps');
 var appSteps = function() {
 
-    this.Given(/^this is the first app$/, function () {
-        console.log('\n----' + this.prop);
-    });
-
-    this.Given(/^this is the second app$/, function () {
-        return this.greetings('everybody');
-    });
-
     this.Given(/^this is the first app$/, function (callback) {
         // Write code here that turns the phrase above into concrete actions
         callback(null, 'pending');
